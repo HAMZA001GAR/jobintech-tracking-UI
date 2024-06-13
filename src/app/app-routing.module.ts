68d286ab3c7d8f53235a6fca15eb7305f39aa411
Nadia Outizi <107075774+NadiaOutizi@users.noreply.github.com>
2024-06-13 14:49:59 +0100

    merged

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormationsComponent } from './ExpertDashboards/formations/formations.component';
+import { StepsComponent } from './ExpertDashboards/steps/steps.component';
+import { ProgressCardComponent } from './ExpertDashboards/progress-card/progress-card.component';
+import { TaskBoardComponent } from './ExpertDashboards/task-board/task-board.component';
+import { AddCategoryComponent } from './ExpertDashboards/add-category/add-category.component';
+import { EditStepComponent } from './ExpertDashboards/edit-step/edit-step.component';
+
+const routes: Routes = [
+  { path: 'formations', component: FormationsComponent },
+  { path: 'steps/:formationId', component: StepsComponent },
+  { path: 'parcours', component: ProgressCardComponent },
+  { path: 'steps', component: TaskBoardComponent },
+  { path: 'add-category', component: AddCategoryComponent },
+  { path: 'edit-step', component: EditStepComponent},
+  { path: '', redirectTo: '/parcours', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormationsComponent } from './ExpertDashboards/formations/formations.component';
+import { StepsComponent } from './ExpertDashboards/steps/steps.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { ProgressCardComponent } from './ExpertDashboards/progress-card/progress-card.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { TaskBoardComponent } from './ExpertDashboards/task-board/task-board.component';
+import { TaskCardComponent } from './ExpertDashboards/task-card/task-card.component';
+import { EditStepComponent } from './ExpertDashboards/edit-step/edit-step.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormationsComponent,
+    StepsComponent,
+    SidebarComponent,
+    ProgressCardComponent,
+    HeaderComponent,
+    TaskBoardComponent,
+    TaskCardComponent,
+    EditStepComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
